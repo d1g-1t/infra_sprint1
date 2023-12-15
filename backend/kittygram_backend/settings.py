f0c2e79f57640a0ae61a1a33c60a5755031a44b8
d1g_1t <paokhrim@gmail.com>
2023-12-15 08:50:36 +0000

    Minor fixes.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['158.160.7.192', '127.0.0.1', 'localhost', 'kittygram-project.bounceme.net']
 
